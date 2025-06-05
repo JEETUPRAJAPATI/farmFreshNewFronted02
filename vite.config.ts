bcc7c1ab81f7ce17f0d2f67305b80ad788cda9a9
jeetu-mj <jeetu.mj123@gmail.com>
2025-06-05 15:44:56 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -11,21 +11,21 @@ export default defineConfig({
     process.env.REPL_ID !== undefined
       ? [
           await import("@replit/vite-plugin-cartographer").then((m) =>
-            m.cartographer(),
+            m.cartographer()
           ),
         ]
       : []),
   ],
   resolve: {
     alias: {
-      "@": path.resolve(import.meta.dirname, "client", "src"),
-      "@shared": path.resolve(import.meta.dirname, "shared"),
-      "@assets": path.resolve(import.meta.dirname, "attached_assets"),
+      "@": path.resolve(__dirname, "src"),
+      "@shared": path.resolve(__dirname, "shared"),
+      "@assets": path.resolve(__dirname, "attached_assets"),
     },
   },
-  root: path.resolve(import.meta.dirname, "client"),
+  root: ".", // <- now root is current folder
   build: {
-    outDir: path.resolve(import.meta.dirname, "dist"),
+    outDir: "dist", // <- not dist/public
     emptyOutDir: true,
-  }
+  },
 });
